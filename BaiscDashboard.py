ac7cc7ee4f813e2f5b9055d387f0a53c884b865c
javariaqayyum <97040671+javariaqayyum@users.noreply.github.com>
2023-03-01 12:29:44 +0500

    Added one more visual and trying to figure out added more column.

--- BaiscDashboard.py
+++ BaiscDashboard.py
@@ -10,18 +10,26 @@ data = pd.read_csv("Data/Data - DS C1 Course.csv")
 
 fig = px.bar(data, x="Age", y="Are you currently attending University / College?", color="Payment", barmode="group")
 fig1 = px.bar(data, x='Payment', y='Age', color= "Which City are you currently residing in?")
+fig2 = px.pie(data, values='Age', names='Gender')
 
 app.layout = html.Div(children=[
-    html.H1('Course Dashboard'),
-    dcc.Graph(id="graph1", 
-        figure=fig,
-        style={'display': 'inline-block'}
-        
-    ), 
-    dcc.Graph(id="graph1", 
-        figure=fig1,
-        style={'display': 'inline-block'}
-    )
+    html.Div([
+     dcc.Graph(id="graph1", figure=fig), 
+     dcc.Graph(id="gra7", figure=fig2)
+     ],className='column'),
+    
+    html.Div([
+     dcc.Graph(id="graph2", figure=fig1), 
+     ],className='row'),
+
+    
+    html.Div([
+     dcc.Graph(id="graph3", figure=fig), 
+     ],className='Column'),
+
+    html.Div([
+     dcc.Graph(id="graph4", figure=fig1), 
+     ],className='Column'),
 
 ])
 
