8861845355cb3789e4735137ff341229f12a69e6
javariaqayyum <97040671+javariaqayyum@users.noreply.github.com>
2023-03-01 17:26:37 +0500

    Added few visualz and trying to add some sidebar and reactive thing that doesn't work properly.

--- BaiscDashboard.py
+++ BaiscDashboard.py
@@ -4,37 +4,71 @@ from dash import html
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
-app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data = pd.read_csv("Data/Data - DS C1 Course.csv")
 
 fig = px.bar(data, x="Age", y="Are you currently attending University / College?", color="Payment", barmode="group")
 fig1 = px.bar(data, x='Payment', y='Age', color= "Which City are you currently residing in?")
 fig2 = px.pie(data, values='Age', names='Gender')
+fig3 = px.histogram(data, x='Age', y='Age',
+             hover_data=['Timestamp'], color='Do you understand that this is a paid course ?',
+             labels={}, height=400)
 
-app.layout = html.Div(children=[
-    html.Div([
-     dcc.Graph(id="graph1", figure=fig), 
-     dcc.Graph(id="gra7", figure=fig2)
-     ],className='six columns'),
-    
-    html.Div([
-     dcc.Graph(id="graph2", figure=fig1), 
-     ],className='six columns'),
+# Define the sidebar content
+sidebar = html.Div(
+    [
+        html.H2("Sidebar"),
+        html.Hr(),
+        html.P("Sidebar content goes here..."),
+    ],
+    id="sidebar",
+    className="sidebar",
+)
 
-    
-    html.Div([
-     dcc.Graph(id="graph3", figure=fig), 
-     ],className='six columns'),
+# Define the main content
+content = html.Div(
+    [
+        html.Div(
+            [
+                dcc.Graph(id="graph1", figure=fig), 
+                dcc.Graph(id="gra7", figure=fig2)
+            ],
+            className="column"
+        ),
+        html.Div(
+            [
+                dcc.Graph(id="graph2", figure=fig1)
+            ],
+            className="column"
+        ),
+        html.Div(
+            [
+                dcc.Graph(id="graph3", figure=fig3)
+            ],
+            className="column"
+        ),
+        html.Div(
+            [
+                dcc.Graph(id="graph4", figure=fig1)
+            ],
+            className="column"
+        ),
+    ],
+    className="main-content",
+)
 
-    html.Div([
-     dcc.Graph(id="graph4", figure=fig1), 
-     ],className='six columns'),
+# Define the layout
+app.layout = html.Div([sidebar, content])
 
-])
+# Define the CSS styles
+app.css.append_css(
+    {
+        "external_url": "https://raw.githubusercontent.com/saadmk11/dash-css-styles/main/styles.css"
+    }
+)
 
 if __name__ == '__main__':
     app.run_server(host='localhost',port=8005)
-
-
